490ac4a2f51588253196f39fc59acfcc6d8860d0
DrKlonk <joranminjon@gmail.com>
2021-02-06 10:13:23 +0100

    init add memory page

--- src/pages/AddMemoryPage.vue
+++ src/pages/AddMemoryPage.vue
@@ -1,12 +1,32 @@
 <template>
   <base-layout page-title="Add a memory" page-default-back-link='/memories'>
-    <h2>Add a memory</h2>
+    <ion-list>
+        <ion-item>
+            <ion-label position="floating">Title</ion-label>
+            <ion-input type="text" required />
+        </ion-item>
+        <ion-item>
+            <ion-label position="floating">Image URL</ion-label>
+            <ion-input type="url" required />
+        </ion-item>
+        <ion-item>
+            <ion-label position="floating">Description</ion-label>
+            <ion-textarea rows="5"></ion-textarea>
+        </ion-item>
+    </ion-list>
   </base-layout>
 </template>
 
 <script>
+import { IonItem, IonInput, IonLabel, IonList, IonTextarea} from '@ionic/vue'
 export default {
-
+    components: {
+        IonInput,
+        IonItem,
+        IonLabel,
+        IonList,
+        IonTextarea,
+    }
 }
 </script>
 
--- src/pages/MemoriesPage.vue
+++ src/pages/MemoriesPage.vue
@@ -1,7 +1,7 @@
 <template>
     <base-layout page-title="Memories">
         <template v-slot:actions-end>
-            <ion-button>
+            <ion-button router-link='/memories/add'>
                 <ion-icon slot="icon-only" :icon=add>
                 </ion-icon>
             </ion-button>
