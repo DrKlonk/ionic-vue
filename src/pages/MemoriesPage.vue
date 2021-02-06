a4e29970aae0cde7b8c2ceed45e1662d060ac928
DrKlonk <joranminjon@gmail.com>
2021-02-06 10:01:12 +0100

    add addMemory button

--- src/pages/MemoriesPage.vue
+++ src/pages/MemoriesPage.vue
@@ -1,13 +1,28 @@
 <template>
     <base-layout page-title="Memories">
+        <template v-slot:actions-end>
+            <ion-button>
+                <ion-icon slot="icon-only" :icon=add>
+                </ion-icon>
+            </ion-button>
+        </template>
         <memories-list :memories="memories"/>
     </base-layout>
 </template>
 
 <script>
+import { IonButton, IonIcon } from '@ionic/vue'
+import { add } from 'ionicons/icons'
 import MemoriesList from '../components/memories/MemoriesList'
 export default {
+    data() {
+        return {
+            add
+        }
+    },
     components: {
+        IonButton,
+        IonIcon,
         MemoriesList
     },
     computed: {
